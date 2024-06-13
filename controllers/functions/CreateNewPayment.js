53f99602878217b04ff833cd1750ea08c8f6d1b3
Robert Modi <rmodiah@gmail.com>
2024-06-13 11:55:10 +0000

    Ready to deply

---
+++ controllers/functions/CreateNewPayment.js
@@ -0,0 +1,17 @@
+function CreateNewPayment(fees_schedule) {
+    const feespayments = [];
+    let total_paid = 0;
+    fees_schedule.forEach(payment => {
+        total_paid += payment.amount_paid;
+        const paid = {
+            "amount_paid": payment.amount_paid,
+            "date_paid": payment.date_paid
+        };
+        feespayments.push(paid);
+    });
+    return {
+        total: total_paid,
+        payments: { ...feespayments }
+    };
+}
+exports.CreateNewPayment = CreateNewPayment;
